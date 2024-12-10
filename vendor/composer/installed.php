cb57e929cd9b9f38f54c3dd09cb5eecc1d13c6c0
Ignacio Gonzalez <101266032+nashgzzz@users.noreply.github.com>
2024-12-10 03:31:54 -0800

    m

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'almasaeed2010/adminlte',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '4f9b29d494f36b74aec4b8f0147b9632c11f4345',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'almasaeed2010/adminlte' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '4f9b29d494f36b74aec4b8f0147b9632c11f4345',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
